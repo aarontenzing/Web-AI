0e025e08410892222b5f3e2577162fa649f84502
aarontenzing <aarontenzing@gmail.com>
2024-01-06 11:45:41 +0100

    added music class

--- preprocess_data.py
+++ preprocess_data.py
@@ -43,7 +43,6 @@ def preprocess_tiktok():
     ]
     print(df['instrumentalness'])
     df[columns_to_multiply] = df[columns_to_multiply].mul(100).round(1)
-
     df = df.rename(columns={"duration_ms":"duration"})
     df.astype({'duration': 'float64'}).dtypes
     df['duration'] = df['duration'].div(1000)
@@ -61,6 +60,7 @@ def create_artist_table(data):
     return artist_table
     
 def create_track_table(data, dict_id_name_mapping):
+    
     data['artist_name'] = data['artist_name'].apply(lambda x: dict_id_name_mapping.get(x[0], "unknown"))
     tracks = data.drop_duplicates(subset=['track_name']).copy() # drop duplicates
     track_id = [idx for idx, _ in enumerate(tracks['track_name'])]
@@ -77,18 +77,27 @@ def create_chart_table(data):
     sort_chart = chart.sort_values(by=['in_spotify_charts', 'in_apple_charts', 'in_deezer_charts', 'in_shazam_charts'], ascending=False)
     chart_id = [idx for idx, _ in enumerate(chart.loc[:, ('track_id')])]
     sort_chart.insert(0, 'chart_id', chart_id)
-    sort_chart = sort_chart.rename(columns={"track_id":"track"})
     sort_chart.to_csv('charts.csv',index=False) # write charts to csv
     
 def create_playlist_table(data):
     playlist = data.loc[:, ('track_id','in_spotify_playlists', 'in_apple_playlists', 'in_deezer_playlists')]
     playlist.dropna(subset=['in_spotify_playlists', 'in_apple_playlists', 'in_deezer_playlists'], inplace=True)
-    playlist['in_deezer_playlists'] = playlist['in_deezer_playlists'].str.replace(',', '').astype(int)
+    playlist['in_deezer_playlists'] = playlist['in_deezer_playlists'].str.replace(',', '').astype(int) # , removen met str.replace
+    playlist['in_spotify_playlists'] = playlist['in_spotify_playlists'].astype(int) # .0 removen met astype(int)
+    playlist['in_apple_playlists'] = playlist['in_apple_playlists'].astype(int) # .0 removen met astype(int)
     sort_playlist = playlist.sort_values(by=['in_spotify_playlists', 'in_apple_playlists', 'in_deezer_playlists'], ascending=False)
     playlist_id = [idx for idx, _ in enumerate(playlist.loc[:, ('track_id')])]
     sort_playlist.insert(0, 'playlist_id', playlist_id)
-    sort_playlist = sort_playlist.rename(columns={"track_id":"track"})
     sort_playlist.to_csv('playlists.csv',index=False) # write playlists to csv
+
+def create_music_table(data):
+    music = data.loc[:, ('track_id','danceability', 'energy', 'key', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo')]
+    music_id = [idx for idx, _ in enumerate(music.loc[:, ('track_id')])]
+    music.insert(0, 'music_id', music_id)
+    music.fillna(-1, inplace=True)
+    music = music.astype(int, errors='ignore') # .0 removen met
+    music['mode'] = music['mode'].replace({0: "major", 1: "minor"}) # 0 naar False en 1 naar True mode
+    music.to_csv('music.csv',index=False) # write music to csv
     
 if __name__ == "__main__":
     
@@ -112,6 +121,9 @@ if __name__ == "__main__":
     
     # Create Playlist table
     create_playlist_table(tracks)
+    
+    # Create Music table
+    create_music_table(tracks)
     
     
     
